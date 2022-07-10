2d835f0989c0ef85ee2470001de44c330b18b96c
apaudel96 <apaudel96@ymail.com>
2022-07-10 12:02:33 +0100

    Initiate the frontend.

---
+++ frontend/src/routes/__layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<link
+		rel="stylesheet"
+		type="text/css"
+		href="https://cdn.jsdelivr.net/npm/fomantic-ui@2.8.8/dist/semantic.min.css"
+	/>
+</svelte:head>
+<main>
+	<slot />
+</main>
